c949861ccb16d3f902ab75ed931a17e0bf66029c
LordFildzhion <dboechnick1@mail.ru>
2024-12-15 13:29:27 +0700

    Add tests for circular buffer

--- lab1-b/bin/main.cpp
+++ lab1-b/bin/main.cpp
@@ -1,29 +1,8 @@
-#include <iostream>
-#include <algorithm>
-#include <concepts>
+#include "lib/circularbuffer.hpp"
 
-
-#include "lib/circularbuffer/circularbuffer.hpp"
-
-using namespace std;
 
 int main()
 {
-    CircularBuffer<int> a(10, 5);
-    for (int i = 0; i < 15; i ++)
-    {
-        for (int j = 0; j < a.size(); j++)
-            cout << a[j] << " ";
-        cout << endl;
-        a.push_back(i);
-    }
-    for (int i = 0; i < 11; i ++)
-    {
-        for (int j = 0; j < a.size(); j++)
-            cout << a[j] << " ";
-        cout << endl;
-        a.pop_back();
-    }
-    for (int j = 0; j < a.size(); j++)
-        cout << a.at(j) << " ";
+    // Create a circular buffer of integers with a capacity of 10.
+    // ...
 }
